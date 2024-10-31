1d89b52e845a6b1409f26d0b0123b58f6002c131
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2024-10-30 20:22:07 -0700

    Finished the Server/PORT code in JavaScript. Remember to do 'node app.js' and click the port link to open the page.

--- app.js
+++ app.js
@@ -8,11 +8,25 @@ app.use(express.urlencoded({ extended: false }));
 app.set('view engine','ejs');
 
 // Still working on this
-app.get('/', )
+app.get('/', (req,res) => {
+    console.log("Home Page - Server Side");
+    res.render('home');
+});
+
+app.get('/', (req,res) => {
+    console.log(req.body);
+
+    let details = req.body;
+    res,render('confirm', {details: details});
+});
 
 app.post('/', (req, res) => {
-    if (req.body.numer) {
+    if (req.body.number) {
         let number = req.body.number;
         res.render('home', { message: `You entered ${number}`});
     }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server runnin on port http://localhost:${3000}`)
 });
